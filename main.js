61163e0c9521e2d17ea69f81527b2e07a2582cf7
Ritik <raj769417@gmail.com>
2023-04-14 13:49:40 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const path = require('path');
+const {v4: uuid} = require('uuid');
+const methodOverride = require('method-override');
+
+
+const app = express();
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('static'));
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended: true}));
+
+
+let comments = [
+    {
+        sn: uuid(),
+        comment: 'Chlo movie dekhne chlte hain',
+        user: 'Ritik'
+    },
+    {
+        sn: uuid(),
+        comment: 'John wick 4 dekhte hai',
+        user: 'Shubham'
+    },
+    {
+        sn: uuid(),
+        comment: 'Waise Avatar 2 bhi release ho chuki h',
+        user: 'Utkarsh'
+    },
+    {
+        sn: uuid(),
+        comment: 'Pehle bahar chlte h phit decide kr lege',
+        user: 'Kshitij'
+    }
+]
+
+app.get('/comments', (req, res) => {
+    res.render('index', { comments });
+})
+
+app.get('/comments/show/:id', (req, res) => {
+    const { id } = req.params;
+    const requested_comment = comments.find(comment => comment.sn===id);
+    res.render('comment', { ...requested_comment });
+})
+
+app.get('/comments/new', (req, res) => {
+    res.render('create');
+})
+
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const requested_comment = comments.find(comment => comment.sn===id);
+    const index = comments.indexOf(requested_comment);
+
+    comments.splice(index, 1);
+
+    res.redirect('/comments');
+
+})
+
+app.post('/comments', (req, res) => {
+    const comment = req.body;
+    comment.sn = uuid();
+    comments.push(comment);
+    
+    res.redirect('/comments');
+})
+
+app.get('*', (req, res) => {
+    res.send("<h1> Not a valid route !!! </h1>");
+})
+
+app.listen(1234, () => {
+    console.log("Listening on  port 1234...");
+})
